ba316f3c7933595cb7ac2ce2a7d64d75eee92b44
JEREMY BALAGTAS <jeremy.balagtas@ibm.com>
2020-02-26 18:19:43 +0800

    added scripts

---
+++ script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+yum install -y wget git
+wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+rpm -ivh epel-release-latest-7.noarch.rpm
+printf '[dockerrepo] \nname=Docker Repository \nbaseurl=https://yum.dockerproject.org/repo/main/centos/7 \nenabled=1 \ngpgcheck=1 \ngpgkey=https://yum.dockerproject.org/gpg' > /etc/yum.repos.d/docker.repo
+until [[ $(rpm -qa docker-engine | wc -l) -gt 0 ]]; do yum -y install docker-engine; echo \"Waiting until docker is installed..\"; sleep 5; done
+service docker start
+chkconfig docker on
+docker run --name mynginx1 --restart always -p 80:80 -v nginx:/usr/share/nginx/html/ -P -d jembim/nginx-aza:0.1
